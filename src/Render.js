c76074dbea90f22626de2f6ab78c3d33988eaa0e
Fernando Servín <tarzanboycraft@hotmail.com>
2019-11-14 16:23:14 -0600

    coordenadas movibles

--- src/Render.js
+++ src/Render.js
@@ -5,22 +5,20 @@ const canvas = document.querySelector('#main')
 canvas.width = window.innerWidth
 canvas.height = window.innerHeight
 const ctx = canvas.getContext('2d')
-let offsetX = window.innerWidth / 2
-let offsetY = window.innerHeight / 2
-ctx.translate(offsetX , offsetY)
+let offsetX = 0
+let offsetY = 0
+ctx.translate(window.innerWidth / 2 , window.innerHeight / 2)
 window.onresize = function(){
     canvas.width = window.innerWidth
     canvas.height = window.innerHeight
-    offsetX = window.innerWidth / 2
-    offsetY = window.innerHeight / 2
-    ctx.translate(offsetX , offsetY)
+    ctx.translate((window.innerWidth / 2)+offsetX , (window.innerHeight / 2)+offsetY)
 }
 
 
 export class Render extends Timer{
     constructor(fps) {
         super(fps)
-        this.map = new Map(15,15)
+        this.map = new Map(10,10)
         this.load()
         this.input = new Input()
     }
@@ -47,40 +45,50 @@ export class Render extends Timer{
             ctx.setTransform(1, 0, 0, 1, 0, 0)
             ctx.clearRect(0, 0, canvas.width, canvas.height)
             ctx.fillStyle = 'rgba(211, 84, 0,1.0)'
-            ctx.fillRect(0, 0, canvas.width, canvas.height);
+            ctx.fillRect(0, 0, canvas.width, canvas.height)
             ctx.restore()
 
 
-
-            ctx.textAlign = 'right';
-            ctx.font = "24px Arial";
-            
+            //COORDENADAS
+            ctx.textAlign = 'right'
+            ctx.font = "24px Arial"
             ctx.textBaseline = 'top'
-            let convertidas = this.map.car2iso((this.input.mouseX-canvas.width/2)/64, (this.input.mouseY-canvas.height/2)/64)
-            // console.log(convertidas)
+            let convertidas = this.map.car2iso((this.input.mouseX-canvas.width/2-offsetX)/64, (this.input.mouseY-canvas.height/2-offsetY)/64)
             let equis = Math.floor(convertidas.x)
             let lle  =Math.floor(convertidas.y)
-            ctx.fillText('X: '+equis, 'Y:'+lle, canvas.width/2, -canvas.height/2)
             if(equis >= 0 && equis < this.map.tiles[0].length && lle >= 0 && lle < this.map.tiles.length){
-                ctx.fillText('X: '+equis+', Y: '+lle, canvas.width/2, -canvas.height/2)
+                ctx.fillText('X: '+equis+ 'Y:'+lle, canvas.width/2-offsetX, -canvas.height/2-offsetY)
+                
                 this.map.tiles[lle][equis].hide = true
             }
+
+
             this.map.draw(ctx)
             if(equis >= 0 && equis < this.map.tiles[0].length && lle >= 0 && lle < this.map.tiles.length){
                 this.map.tiles[lle][equis].hide = false
             }
             ctx.textBaseline = 'bottom'
-            ctx.fillText(this.getFPSCount()+"   FPS", canvas.width/2, canvas.height/2);
+            ctx.fillText(this.getFPSCount()+"   FPS | OffX: "+offsetX+" OffY: "+offsetY, canvas.width/2-offsetX, canvas.height/2-offsetY);
+
+
             // console.log(this.getTime())
-            // console.log(offsetX, offsetY)
-            if(this.input.controls[0])
-                ctx.translate(0 , -3)
-            if(this.input.controls[1])
-                ctx.translate(-3 , 0)
-            if(this.input.controls[2])
-                ctx.translate(0 , 3)
-            if(this.input.controls[3])
-                ctx.translate(3 , 0)
+            let eso = 3
+            if(this.input.controls[0]){
+                offsetY -= eso
+                ctx.translate(0 , -eso)
+            }
+            if(this.input.controls[1]){
+                offsetX -= eso
+                ctx.translate(-eso , 0)
+            }
+            if(this.input.controls[2]){
+                offsetY += eso
+                ctx.translate(0 , eso)
+            }
+            if(this.input.controls[3]){
+                offsetX += eso
+                ctx.translate(eso , 0)
+            }
             
         }
     }
